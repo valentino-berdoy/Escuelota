acde24e64ecf40c35b6c7d0e36dccbe5b7ce85ab
valentino-berdoy <berdoy.valentinoet36@gmail.com>
2023-09-10 20:15:15 -0300

    terminado casi

--- index.js
+++ index.js
@@ -1,92 +1,95 @@
-const x ="X";
-const o ="O";
+const x = "X";
+const o = "O";
 let estadoJuego = "P1";
 const modal = document.querySelector("dialog");
-const textoModal = modal.querySelector("h2")
+const textoModal = modal.querySelector("h2");
 
 const cuadrados = Array.from(document.querySelectorAll(".cuadrado"));
 
-cuadrados.forEach((cuadrado,i) => {
-    cuadrado.addEventListener("click", ()=>{
-        if(estadoJuego === "pausa") return;
-        if(cuadrado.textContent !== "") return;
+cuadrados.forEach((cuadrado, i) => {
+  cuadrado.addEventListener("click", () => {
+    if (estadoJuego != "pausa") {
+      if (cuadrado.textContent !== "") return;
 
-        if (estadoJuego === "P1") {
-            cuadrado.innerText = "X";
-          } else {
-            cuadrado.innerText = "O";
-          }
-        
-        const posicionGanadora = siSeGano();
-        if(typeof posicionGanadora === "object"){
-           quienGano(posicionGanadora);
-           return
-        }
-        if(posicionGanadora === "empate"){
-            mostrarModal("Empataron rey");
-        }
-        if (estadoJuego === "P1") {
-            estadoJuego = "P2";
-          } else {
-            estadoJuego = "P1";
-          }
-          
-    })
-})
+      if (estadoJuego === "P1") {
+        cuadrado.innerText = "X";
+      } else {
+        cuadrado.innerText = "O";
+      }
 
-
-function siSeGano(){
-    const tablero = cuadrados.map(cuadrado => cuadrado.textContent)
-    console.log(tablero)
-
-    //horizontales
-    for (let i = 0; i <= 9; i+= 3) {
-        if(tablero[i] && tablero[i]=== tablero[i+1] && tablero[i] === tablero[i+2]){
-            return ([i, i+1, i+2]);
-        }
-
+      const posicionGanadora = siSeGano();
+      if (typeof posicionGanadora === "object") {
+        quienGano(posicionGanadora);
+        return;
+      }
+      if (posicionGanadora === "empate") {
+        mostrarModal("Empataron rey");
+      }
+      if (estadoJuego === "P1") {
+        estadoJuego = "P2";
+      } else {
+        estadoJuego = "P1";
+      }
     }
+  });
+});
 
-    //verticales
-    for (let i = 0; i <= 9; i+= 1) {
-        if(tablero[i] && tablero[i] === tablero[i+3] && tablero[i] === tablero[i+6]){
-            return ([i, i+3, i+6]);
-        }
+function siSeGano() {
+  const tablero = cuadrados.map(cuadrado => cuadrado.textContent);
+  console.log(tablero);
 
+  //horizontales
+  for (let i = 0; i <= 9; i += 3) {
+    if (
+      tablero[i] &&
+      tablero[i] === tablero[i + 1] &&
+      tablero[i] === tablero[i + 2]
+    ) {
+      return [i, i + 1, i + 2];
     }
+  }
 
-    //oblicuas
-    if(tablero[0] && tablero[0] === tablero[4] && tablero[0] === tablero[8]){
-        return ([0,4,8]);
+  //verticales
+  for (let i = 0; i <= 9; i += 1) {
+    if (
+      tablero[i] &&
+      tablero[i] === tablero[i + 3] &&
+      tablero[i] === tablero[i + 6]
+    ) {
+      return [i, i + 3, i + 6];
     }
-    if(tablero[2] && tablero[2] === tablero[4] && tablero[2] === tablero[6]){
-        return ([2,4,6]);
-    }
-    
-    if(tablero.includes("")) return false;
-    return "empate"
+  }
+
+  //oblicuas
+  if (tablero[0] && tablero[0] === tablero[4] && tablero[0] === tablero[8]) {
+    return [0, 4, 8];
+  }
+  if (tablero[2] && tablero[2] === tablero[4] && tablero[2] === tablero[6]) {
+    return [2, 4, 6];
+  }
+
+  if (tablero.includes("")) return false;
+  return "empate";
 }
 
-function quienGano(posicionGanadora){
-    console.log("ganador ", posicionGanadora);
-    posicionGanadora.forEach(posicion => {
-        cuadrados[posicion].classList.toggle("ganador", true);
-    })
-    mostrarModal("Gano el Jugador: " + estadoJuego);
-    estadoJuego = "pausa";
+function quienGano(posicionGanadora) {
+  console.log("ganador ", posicionGanadora);
+  posicionGanadora.forEach(posicion => {
+    cuadrados[posicion].classList.toggle("ganador", true);
+  });
+  mostrarModal("Gano el Jugador: " + estadoJuego);
+  estadoJuego = "pausa";
 }
 
-function mostrarModal(texto){
-    textoModal.innerText = texto;
-    modal.showModal();
-
+function mostrarModal(texto) {
+  textoModal.innerText = texto;
+  modal.showModal();
 }
-modal.querySelector("button").addEventListener("click",() =>{
-    cuadrados.forEach(cuadrado => {
+modal.querySelector("button").addEventListener("click", () => {
+  cuadrados.forEach(cuadrado => {
     cuadrado.textContent = "";
     cuadrado.classList.toggle("ganador", false);
     modal.close();
-    estadoJuego = "P1"
+    estadoJuego = "P1";
+  });
 });
-
-})
